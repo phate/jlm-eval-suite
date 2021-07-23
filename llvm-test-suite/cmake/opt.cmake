c4752927b47ce05fae9f4f23bcd5b5e97d4dd319
Magnus Sjalander <work@sjalander.com>
2021-07-23 09:25:36 +0200

    llvm-test: Added CNE optimization to llvm-build-opt

--- llvm-test-suite/cmake/opt.cmake
+++ llvm-test-suite/cmake/opt.cmake
@@ -1,4 +1,6 @@
-set(OPTFLAGS "${OPTFLAGS} -Jinv -Jdne -Jivt -Jinv -Jdne -Jinv -Jdne -Jpll -Jinv -Jdne -Jurl -Jinv")
+# O3 = --iln --inv --red --dne --ivt --inv --dne --psh --inv --dne
+#      --red --cne --dne --pll --inv --dne --url --inv
+set(OPTFLAGS "${OPTFLAGS} -Jinv -Jdne -Jivt -Jinv -Jdne -Jinv -Jdne -Jcne -Jdne -Jpll -Jinv -Jdne -Jurl -Jinv")
 
 set(CMAKE_C_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELEASE "${OPTFLAGS}" CACHE STRING "")
