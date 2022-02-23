f96afae1b548a77b3b6ff956f33a33bc0b7b7db3
Magnus Sjalander <work@sjalander.com>
2022-02-23 14:20:30 +0100

    hls-test-suite: Added more test cases, bumped jlm
    The add test has been extended to perform more arithmetic operations.
    Added test with if and else statements as well as equality operators
    (i.e, less than, greater than, equal etc.).

---
+++ hls-test-suite/src/test-arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+int run(int a, int b){
+	int c = ((a + b) * b - a) / b % a;
+	return c;
+}
+
+int main(int argc, char** argv){
+	int result = run(4, 8);
+	printf("Result: %i\n", result);
+
+	// Check if correct result
+	if (result == 3)
+		return 0;
+
+	return 1;
+}
